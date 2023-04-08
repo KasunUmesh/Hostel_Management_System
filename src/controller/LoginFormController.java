230ead13025823abe6e42c8f590e15ff6bdf7514
KasunUmesh <kasunumesh38@gmail.com>
2023-04-08 12:34:16 +0530

    Create Login Page UI

--- src/controller/LoginFormController.java
+++ src/controller/LoginFormController.java
@@ -1,4 +1,24 @@
 package controller;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
 public class LoginFormController {
+    public JFXTextField txtUserName;
+    public JFXPasswordField txtHidePassword;
+    public JFXTextField txtShowPassword;
+    public ImageView imgShowPasswordClick;
+    public ImageView imgHidePasswordClick;
+
+    public void btnLoginOnAction(ActionEvent actionEvent) {
+    }
+
+    public void imgShowEyerClick(MouseEvent mouseEvent) {
+    }
+
+    public void imgHideEyerClick(MouseEvent mouseEvent) {
+    }
 }
