2bd3a60f85b920b6c2730443ed02aabd613ee49a
KasunUmesh <kasunumesh38@gmail.com>
2023-04-08 14:52:49 +0530

    Create Login Page Login Button Function

--- src/controller/LoginFormController.java
+++ src/controller/LoginFormController.java
@@ -3,9 +3,16 @@ package controller;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginFormController {
     public JFXTextField txtUserName;
@@ -13,6 +20,7 @@ public class LoginFormController {
     public JFXTextField txtShowPassword;
     public ImageView imgShowPasswordClick;
     public ImageView imgHidePasswordClick;
+    public AnchorPane root;
 
     String password;
 
@@ -23,7 +31,13 @@ public class LoginFormController {
         imgHidePasswordClick.setVisible(false);
     }
 
-    public void btnLoginOnAction(ActionEvent actionEvent) {
+    public void btnLoginOnAction(ActionEvent actionEvent) throws IOException {
+        Parent parent = FXMLLoader.load(this.getClass().getResource("../view/DashBoardForm.fxml"));
+        Scene scene = new Scene(parent);
+
+        Stage primaryStage = (Stage) this.root.getScene().getWindow();
+        primaryStage.setScene(scene);
+        primaryStage.centerOnScreen();
     }
 
     public void imgShowEyerClick(MouseEvent mouseEvent) {
