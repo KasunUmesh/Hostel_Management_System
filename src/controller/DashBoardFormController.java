2b436ea730b12906fa36034472d8db35b9fb8bc3
KasunUmesh <kasunumesh38@gmail.com>
2023-04-14 00:29:48 +0530

    Create Rooms Management Page

--- src/controller/DashBoardFormController.java
+++ src/controller/DashBoardFormController.java
@@ -1,4 +1,50 @@
 package controller;
 
+import animatefx.animation.FadeIn;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.StackPane;
+
+import java.io.IOException;
+
 public class DashBoardFormController {
+    public StackPane rootHome;
+
+    public void initialize(){
+
+
+        try {
+            StackPane pane = null;
+            pane = FXMLLoader.load(this.getClass().getResource("../view/HomePageForm.fxml"));
+            rootHome.getChildren().setAll(pane);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+    public void btnHomeOnAction(ActionEvent actionEvent) throws IOException {
+        StackPane pane = null;
+        pane = FXMLLoader.load(this.getClass().getResource("../view/HomePageForm.fxml"));
+        rootHome.getChildren().setAll(pane);
+
+        new FadeIn(rootHome).play();
+    }
+
+    public void btnAddStudentOnAction(ActionEvent actionEvent) throws IOException {
+        StackPane pane = null;
+        pane = FXMLLoader.load(this.getClass().getResource("../view/AddStudentForm.fxml"));
+        rootHome.getChildren().setAll(pane);
+
+        new FadeIn(rootHome).play();
+    }
+
+    public void btnRoomsOnAction(ActionEvent actionEvent) throws IOException {
+        StackPane pane = null;
+        pane = FXMLLoader.load(this.getClass().getResource("../view/RoomsForm.fxml"));
+        rootHome.getChildren().setAll(pane);
+
+        new FadeIn(rootHome).play();
+    }
 }
