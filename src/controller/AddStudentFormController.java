ea1a03dd2c2e2b652d2fcfdfd01b2ff95519202c
KasunUmesh <kasunumesh38@gmail.com>
2023-04-14 01:59:56 +0530

    Create Student Add And Reservation add UI

--- src/controller/AddStudentFormController.java
+++ src/controller/AddStudentFormController.java
@@ -1,4 +1,15 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.layout.Pane;
+
 public class AddStudentFormController {
+    public Pane pnRoomResDetails;
+    public Pane pnStudentDetails;
+
+    public void btnViewStudentDetails(ActionEvent actionEvent) {
+    }
+
+    public void btnViewResDetails(ActionEvent actionEvent) {
+    }
 }
