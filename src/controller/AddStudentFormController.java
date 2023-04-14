01e388e8d95bd498657e9ba9a2778ae95b8b3193
KasunUmesh <kasunumesh38@gmail.com>
2023-04-14 10:01:14 +0530

    Create Hibernate Properties File

--- src/controller/AddStudentFormController.java
+++ src/controller/AddStudentFormController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import animatefx.animation.ZoomIn;
 import javafx.event.ActionEvent;
 import javafx.scene.layout.Pane;
 
@@ -8,8 +9,14 @@ public class AddStudentFormController {
     public Pane pnStudentDetails;
 
     public void btnViewStudentDetails(ActionEvent actionEvent) {
+        pnStudentDetails.setVisible(true);
+        new ZoomIn(pnStudentDetails).play();
+        pnRoomResDetails.setVisible(false);
     }
 
     public void btnViewResDetails(ActionEvent actionEvent) {
+        pnRoomResDetails.setVisible(true);
+        new ZoomIn(pnRoomResDetails).play();
+        pnStudentDetails.setVisible(false);
     }
 }
