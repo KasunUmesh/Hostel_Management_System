1327dc98642baa0cbb6fce566f67a49e446d5cbe
KasunUmesh <kasunumesh38@gmail.com>
2023-04-14 20:03:00 +0530

    Create RoomBo Class Function

--- src/bo/custom/impl/RoomBOImpl.java
+++ src/bo/custom/impl/RoomBOImpl.java
@@ -7,6 +7,7 @@ import dao.custom.impl.RoomDAOImpl;
 import dto.RoomDTO;
 import entity.Room;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RoomBOImpl implements RoomBO {
@@ -25,16 +26,32 @@ public class RoomBOImpl implements RoomBO {
 
     @Override
     public List<RoomDTO> findAll() {
-        return null;
+
+        List<Room> all = roomDAO.findAll();
+        ArrayList<RoomDTO> roomDTOS = new ArrayList<>();
+        for (Room room : all) {
+            roomDTOS.add(new RoomDTO(
+                    room.getRoom_type_id(),
+                    room.getRoom_type(),
+                    room.getKey_money(),
+                    room.getQty()
+            ));
+        }
+        return roomDTOS;
     }
 
     @Override
     public boolean delete(String id) {
-        return false;
+        return roomDAO.delete(id);
     }
 
     @Override
     public boolean update(RoomDTO roomDTO) {
-        return false;
+        return roomDAO.update(new Room(
+                roomDTO.getRoom_type_id(),
+                roomDTO.getRoom_type(),
+                roomDTO.getKey_money(),
+                roomDTO.getQty()
+        ));
     }
 }
