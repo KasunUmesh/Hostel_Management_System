41c621b13750818f2054ec8935b38fcb862b4d39
KasunUmesh <kasunumesh38@gmail.com>
2023-04-20 19:35:24 +0530

    Create Student DAO Class

--- src/dao/custom/StudentDAO.java
+++ src/dao/custom/StudentDAO.java
@@ -3,5 +3,10 @@ package dao.custom;
 import dao.SuperDAO;
 import entity.Student;
 
+import java.math.BigInteger;
+
 public interface StudentDAO extends SuperDAO<Student, String> {
+    boolean ifStudentExist(String id);
+    String generateNewID();
+    BigInteger studentCount();
 }
--- src/dao/custom/impl/StudentDAOImpl.java
+++ src/dao/custom/impl/StudentDAOImpl.java
@@ -4,8 +4,10 @@ import dao.custom.StudentDAO;
 import entity.Student;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 import util.FactoryConfigration;
 
+import java.math.BigInteger;
 import java.util.List;
 
 public class StudentDAOImpl implements StudentDAO {
@@ -23,12 +25,23 @@ public class StudentDAOImpl implements StudentDAO {
 
     @Override
     public boolean update(Student entity) {
-        return false;
+        Session session = FactoryConfigration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+        session.update(entity);
+        transaction.commit();
+        session.close();
+        return true;
     }
 
     @Override
     public boolean delete(String s) {
-        return false;
+        Session session = FactoryConfigration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+        Student student = session.get(Student.class, s);
+        session.delete(student);
+        transaction.commit();
+        session.close();
+        return true;
     }
 
     @Override
@@ -38,6 +51,53 @@ public class StudentDAOImpl implements StudentDAO {
 
     @Override
     public List<Student> findAll() {
-        return null;
+        Session session = FactoryConfigration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+        Query query = session.createQuery("from Student");
+        List<Student> list = query.list();
+
+        transaction.commit();
+        session.close();
+        return list;
+    }
+
+    @Override
+    public boolean ifStudentExist(String id) {
+        Session session = FactoryConfigration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+        Query query = session.createQuery("SELECT student_ID FROM Student WHERE student_ID=:id");
+        String id1 = (String) query.setParameter("id", id).uniqueResult();
+        if (id1!=null) {
+            return true;
+        }
+        transaction.commit();
+        session.close();
+        return false;
+    }
+
+    @Override
+    public String generateNewID() {
+        Session session = FactoryConfigration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+        Query query = session.createSQLQuery("SELECT student_ID FROM Student ORDER BY student_ID DESC LIMIT 1");
+        String s = (String) query.uniqueResult();
+        transaction.commit();
+        session.close();
+        if (s!=null) {
+            int newStudentID = Integer.parseInt(s.replace("s", "")) + 1;
+            return String.format("S%03d", newStudentID);
+        }
+        return "S001";
+    }
+
+    @Override
+    public BigInteger studentCount() {
+        Session session = FactoryConfigration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+        Query query = session.createSQLQuery("SELECT COUNT(*) FROM Student");
+        BigInteger bigInteger = (BigInteger) query.uniqueResult();
+        transaction.commit();
+        session.close();
+        return bigInteger;
     }
 }
